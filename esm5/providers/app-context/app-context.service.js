dec01bdf094ddcb58e27706247d1bd29afe3a1db
xiang ni <xiang_x_ni@manulife.com>
2022-09-29 12:08:59 +0800

    update build

--- esm5/providers/app-context/app-context.service.js
+++ esm5/providers/app-context/app-context.service.js
@@ -1,3 +1,9 @@
+import { __decorate } from "tslib";
+import { Injectable } from '@fm/di';
 import { AppContextService as SharedAppContextService } from '@fm/shared/providers/app-context';
-export class AppContextService extends SharedAppContextService {
-}
+let AppContextService = class AppContextService extends SharedAppContextService {
+};
+AppContextService = __decorate([
+    Injectable()
+], AppContextService);
+export { AppContextService };
