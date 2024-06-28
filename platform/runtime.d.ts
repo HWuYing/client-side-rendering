553508e97abe2c41c1810ab9df10c922716091cb
xiang ni <xiang_x_ni@manulife.com>
2024-06-28 16:22:38 +0800

    update

--- platform/runtime.d.ts
+++ platform/runtime.d.ts
@@ -11,6 +11,6 @@ declare class DynamicPlatform {
 export { PLATFORM_SCOPE } from '@fm/core/platform/application';
 export declare const dynamicPlatform: (providers?: Provider[]) => DynamicPlatform;
 export { ApplicationPlugin, Input, Prov, registerProvider } from '@fm/core/platform/decorator';
-export declare const Application: <M extends import("@fm/core/platform/application").MetadataInfo>(metadata?: {
+export declare const Application: (metadata?: {
     [key: string]: any;
-} | import("@fm/di").Type<M>) => <T = any>(cls: import("@fm/di").Type<T>) => import("@fm/di").Type<T>;
+} | import("@fm/di").Type<import("@fm/core/platform/application").MetadataInfo>) => import("../../di/decorators").ClassDecorator;
