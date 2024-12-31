dff299edfeec599679ed7e3ca1263a38d4c1cd10
Xiang Ni （倪祥) <Xiang_X_Ni@manulife.com>
2024-12-31 10:37:23 +0800

    update

--- platform/runtime.d.ts
+++ platform/runtime.d.ts
@@ -1,6 +1,6 @@
 import './plugin.effects';
 export { PLATFORM_SCOPE } from '@hwy-fm/core/platform';
 export { ApplicationPlugin, createRegisterLoader, Input, Prov, Register, runtimeInjector } from '@hwy-fm/core/platform/decorator';
-export declare const Application: (metadata?: import("../../di").Type<import("../../core/platform/decorator").MetadataInfo> | {
+export declare const Application: (metadata?: import("@hwy-fm/di").Type<import("@hwy-fm/core/platform/decorator").MetadataInfo> | {
     [key: string]: any;
-}) => ClassDecorator;
+}) => import("../../di/decorators").ClassDecorator<any>;
